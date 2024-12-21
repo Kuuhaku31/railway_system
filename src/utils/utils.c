4daa0c2cd84e4cbbcfd7e687e432892e6e5028f1
孙羽 <public@mikesolar.cn>
2024-12-21 15:34:05 +0800

    处理了几个工具函数Bug

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -62,29 +62,59 @@ int finalizeDb() {
     }
 }
 
-
+/**
+ * 加载配置文件
+ * @return 错误码
+ */
 int initConfig() {
+    //全设为0，防止忘记字符串结尾的'\0'。
     memset(&config, 0, sizeof(Config));
     FILE *fp;
     int ret = SUCCESS;
+    //打开文件
     if ((fp = fopen(CONFIG_FILE, "r")) == NULL) {
+        //打开失败则加载默认配置
         config = defaultConfig;
         return CONFIG_FILE_BROKEN;
     }
     char buffer[MAX_CONFIG_LINE] = {0};
     char prop_buffer[MAX_PROP] = {0};
+    //逐行读取文件
     while (fgets(prop_buffer, MAX_PROP, fp) != NULL) {
+        //首先，把行尾的换行符去掉
+        for(int i = 0; prop_buffer[i]!='\0'; i++)
+        {
+            if (prop_buffer[i] == '\n')
+            {
+                prop_buffer[i] = '\0';
+                break;
+            }
+        }
+        //匹配数据库名称的配置项
         if (strcmp(prop_buffer, DB_FILE_CONFIG) == 0) {
             memset(prop_buffer, 0, sizeof(char) * MAX_PROP);
+            //匹配上了，读取配置
             if (fgets(buffer, MAX_CONFIG_LINE, fp) != NULL) {
-                strcpy_s(config.db_path, strlen(buffer), buffer);
+                //干掉换行符
+                for(int i = 0; buffer[i]!='\0'; i++)
+                {
+                    if (buffer[i] == '\n')
+                    {
+                        buffer[i] = '\0';
+                        break;
+                    }
+                }
+                //全设为零，备用————虽然目前就这一个配置，不存咋备用
+                strcpy_s(config.db_path, MAX_DB_FILE_CONFIG, buffer);
                 memset(buffer, 0, sizeof(char) * MAX_CONFIG_LINE);
             } else {
+                //读取失败的话，说明配置文件损坏，为这一项加载默认配置
                 strcpy_s(config.db_path, MAX_DB_FILE_CONFIG, defaultConfig.db_path);
                 ret = CONFIG_FILE_BROKEN;
             }
         }
     }
+    fclose(fp);
     return ret;
 }
 
